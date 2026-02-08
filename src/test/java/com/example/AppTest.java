5a422d5c0fb607f9b78c31ee0e621a0dbcff29a4
unknown <kenzataktak40@gmail.com>
2026-02-08 12:00:10 +0100

    Init project +JUnit test

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+
+    @Test
+    public void testSum() {
+        int result = App.sum(2, 3);
+        assertEquals(5, result, "2 + 3 = 5");
+    }
+}
